000a36172952549f8df391ff7ade17fe27baa513
Chris Buteau <cbuteau@gmail.com>
2021-07-20 14:51:01 -0400

    maybe

---
+++ build.bat
@@ -0,0 +1,3 @@
+rem cmake neo -DCMAKE_BUILD_TYPE=Release
+
+cmake -DCMAKE_BUILD_TYPE=Release -DZLIB_LIBRARY="C:\Program Files (x86)\GnuWin32\include" -DZLIB_INCLUDE_DIR="C:\Program Files (x86)\GnuWin32\lib" -DOPENAL_INCLUDE_DIR="C:\Program Files (x86)\OpenAL 1.1 SDK\include" -DOPENAL_LIBRARY="C:\Program Files (x86)\OpenAL 1.1 SDK\libs" -DSDL2_INCLUDE_DIR="C:\SDL2-2.0.14\include" -DSDL2_LIBRARY="C:\SDL2-2.0.14\lib" neo
